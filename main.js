7f3477f56e85370c1e254af83460e81499ebc01e
manugatinha <111586152+manugatinhaa@users.noreply.github.com>
2024-04-08 08:59:35 -0300

    style.css

---
+++ main.js
@@ -0,0 +1,13 @@
+cons botoes = document.querySelectorA11("botao")
+
+for(let i=0;i,<botoes. legth; i++){
+    botoes[i].onclick =function(){
+
+    for(let j=0;<botoes.length;j++){
+        botoes[j].classlist.remove("ativo");
+    }
+
+        botoes[i]classlist.add("ativo")
+    }
+    
+}
